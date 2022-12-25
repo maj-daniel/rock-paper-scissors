388a78d1b4562def310996bbdc138217429566f0
maj-daniel <daniel.soareslb@gmail.com>
2022-12-25 20:56:53 -0300

    Feat: Make the game play a round
    Created function to run one round of rock, paper and scissors
    returning 1 to win, 0 to tie and -1 for a lost.

--- js/script.js
+++ js/script.js
@@ -10,3 +10,40 @@ function getComputerChoice (){
             return "scissors";
     }
 }
+
+function playRound (playerSelection, computerSelection) {
+    //if playerChoice is rock, paper or scissors, test each possibility of result
+    //return 1 if win, -1 if lose and 0 for a tie
+    //else, say to player to give a compatible input.
+    if (playerSelection === "rock"){
+        switch (computerSelection) {
+            case "rock":
+                return 0;
+            case "paper":
+                return -1;
+            case "scissors":
+                return 1;
+        }
+    } else if (playerSelection === "paper"){
+        switch (computerSelection) {
+            case "paper":
+                return 0;
+            case "scissors":
+                return -1;
+            case "rocks":
+                return 1;
+        }
+    } else if (playerSelection === "scissors"){
+        switch (computerSelection) {
+            case "scissors":
+                return 0;
+            case "rock":
+                return -1;
+            case "paper":
+                return "Player win";
+        }
+    } else {
+        return "Invalid input! Please try rock, paper or scissors!"
+    }
+
+}
